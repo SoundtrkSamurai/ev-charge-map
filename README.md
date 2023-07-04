ab3c8054cd06551d0becd3714277f1ecaae3cc9d
Allen Taylor <Allen.Taylor@esentire.com>
2023-07-04 15:31:57 -0700

    first commit.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# ev-charge-map
-Quasar demo of app to find route with ev-charging stations. 
+# EV Chage Map (ev-charge-map)
+
+An app to find charge stations along your journey.
+
+## Install the dependencies
+```bash
+yarn
+# or
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+
+### Lint the files
+```bash
+yarn lint
+# or
+npm run lint
+```
+
+
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.config.js](https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js).
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout>
+    <q-header class="bg-teal">
+      <q-toolbar>
+        <q-toolbar-title>EV Charge Map </q-toolbar-title>
+        <q-btn flat dense icon="login" />
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-footer>
+      <q-tabs class="text-light bg-teal">
+        <q-tab name="/map" icon="explore" label="Maps" />
+        <q-tab name="/route" icon="directions" label="Routes" />
+        <q-tab name="/auth" icon="login" label="Auth" />
+      </q-tabs>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup>
+
+</script>
