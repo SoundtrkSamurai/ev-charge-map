0e30c1d416dab96a7d37d0de586db48f7bc28c9a
Allen Taylor <Allen.Taylor@esentire.com>
2023-07-07 08:16:41 -0700

    auth: create login page.

--- src/pages/auth/LoginSection.vue
+++ src/pages/auth/LoginSection.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>
+  <q-page id="loginPage">
+    <q-toolbar class="text-white bg-teal-8">
+      <q-toolbar-title>Login</q-toolbar-title>
+    </q-toolbar>
+    <q-scroll-area class="form-style">
+      <div class="text-h6 text-center q-pt-md">
+        <q-img src="/favicon.ico" height="60px" width="60px" />
+        <div>EV Charge Map</div>
+      </div>
 
-  </div>
+      <div class="q-pa-md q-gutter-md ">
+        <div class="text-center">Login and get extra features!</div>
+
+        <q-input outlined stack-label label="Email">
+          <template v-slot:append>
+            <q-icon name="close" />
+          </template>
+        </q-input>
+        <q-input outlined stack-label label="Password">
+          <template v-slot:append>
+            <q-icon name="close" />
+          </template>
+        </q-input>
+
+        <div>
+          <q-btn color="black" size="lg" class="full-width" label="Register" />
+        </div>
+
+        <div class="q-px-md q-mt-xl text-center">
+          <div class="q-mb-md no-account">Don't have an account?</div>
+          <q-btn color="teal" outline rounded to="/auth/register" size="15px">Register Here</q-btn>
+        </div>
+      </div>
+    </q-scroll-area>
+  </q-page>
 </template>
 
 <script setup>
@@ -9,5 +41,15 @@
 </script>
 
 <style lang="scss">
+  #loginPage {
+    .form-style {
+      margin: 0 auto;
+      height: calc(100vh - 180px);
+      max-width: 500px;
+    }
 
+    .no-account {
+      font-size: 17px;
+    }
+  }
 </style>
