ec80c6a99fc530b39389169f55529eb33b1bc46e
Allen Taylor <Allen.Taylor@esentire.com>
2023-07-07 08:16:57 -0700

    auth: Create register page.

--- src/pages/auth/RegisterSection.vue
+++ src/pages/auth/RegisterSection.vue
@@ -1,7 +1,48 @@
 <template>
-  <div>
+  <q-page id="registrationPage">
+    <q-toolbar class="text-white bg-teal-8">
+      <q-toolbar-title>Register</q-toolbar-title>
+    </q-toolbar>
+    <q-scroll-area class="form-style">
+      <div class="text-h6 text-center q-pt-md">
+        <q-img src="/favicon.ico" height="60px" width="60px" />
+        <div>EV Charge Map</div>
+      </div>
 
-  </div>
+      <div class="q-pa-md q-gutter-md ">
+        <div class="text-center">Register and get extra features!</div>
+
+        <q-input outlined stack-label label="First Name">
+          <template v-slot:append>
+            <q-icon name="close" />
+          </template>
+        </q-input>
+        <q-input outlined stack-label label="Last Name">
+          <template v-slot:append>
+            <q-icon name="close" />
+          </template>
+        </q-input>
+        <q-input outlined stack-label label="Email">
+          <template v-slot:append>
+            <q-icon name="close" />
+          </template>
+        </q-input>
+        <q-input outlined stack-label label="Password">
+          <template v-slot:append>
+            <q-icon name="close" />
+          </template>
+        </q-input>
+        <q-input outlined stack-label label="Confirm Password">
+          <template v-slot:append>
+            <q-icon name="close" />
+          </template>
+        </q-input>
+        <div>
+          <q-btn color="black" size="lg" class="full-width" label="Register" />
+        </div>
+      </div>
+    </q-scroll-area>
+  </q-page>
 </template>
 
 <script setup>
@@ -9,5 +50,11 @@
 </script>
 
 <style lang="scss">
-
+  #registrationPage {
+    .form-style {
+      margin: 0 auto;
+      height: calc(100vh - 180px);
+      max-width: 500px;
+    }
+  }
 </style>
